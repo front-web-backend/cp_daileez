ff86cab23e75fdbedd8c080c02c870770c817371
wen.zhang <jimokanghanchao@gmail.com>
2012-02-09 23:21:51 +0800

    mothod __unicode__

--- mysite/daileez/models.py
+++ mysite/daileez/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Icontype(models.Model):
+    def __unicode__(self):
+        return self.name
     name = models.CharField(max_length=50)
     desc = models.CharField(max_length=200)
     
 class Icons(models.Model):
+    def __unicode__(self):
+        return self.name
     name = models.CharField(max_length=50)
     desc = models.CharField(max_length=200)
     typeid = models.ForeignKey(Icontype)
